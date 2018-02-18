df7f2aec0e3f1b2a4ad4ab8469411c6df70e373d
Igor Kurek <igorkurek96@gmail.com>
2018-02-18 05:23:32 +0100

    Added script for git output colouring

---
+++ enableGitColours.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config --global color.ui always
+git config --global color.branch always
+git config --global color.diff always
+git config --global color.interactive always
+git config --global color.status always
+git config --global color.grep always
+git config --global color.pager true
+git config --global color.decorate always
+git config --global color.showbranch always
